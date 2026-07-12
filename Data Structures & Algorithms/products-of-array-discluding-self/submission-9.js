6792287b42365b07d14626dcb846e45a1b290c0c
Ogunkoya Alfred <140629756+ogunkoyaalfred@users.noreply.github.com>
2026-07-12 15:53:06 +0100

    Add: products-of-array-discluding-self - submission-9

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-9.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let output = []
+
+        let suffix = 1
+        for(let i = nums.length -1 ; i >= 0; i--){
+            output[i] = suffix
+            suffix *= nums[i]
+        }
+
+        let prefix = 1
+        for(let i=0; i < nums.length; i++){
+           output[i] *= prefix
+            prefix *= nums[i]
+        }
+
+        return output
+
+    }
+}
